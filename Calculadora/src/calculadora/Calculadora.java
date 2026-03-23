f34f6c584236421dbbb56938e4540783c3ced6f5
lsancan <lsancan2411@g.educaand.es>
2026-03-23 14:01:44 +0100

    Feat: añadir método dividir con control de división por cero.

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -15,4 +15,10 @@ public class Calculadora {
 		return a * b;
 	}
 	
+	public double dividir(int a, int b) {
+		if (b == 0) {
+			throw new IllegalArgumentException("No se puede dividir por 0.");
+		}
+		return (double)a/b;
+	}
 }
